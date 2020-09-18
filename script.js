8d724bea2c006bd6936e19dd1b64d69086cd4911
Nikolai <nvanbaak@gmail.com>
2020-09-18 13:04:51 -0700

    planner grabs content from localStorage

--- script.js
+++ script.js
@@ -4,20 +4,56 @@ populatePlanner(8, 18);
 
 
 function populatePlanner(startTime, endTime) {
+    // populatePlanner takes a start time and end time (expressed as integers between 1 and 24) and populates the day planner based on stored data
 
-    // create html structure
+    // ==========================
+    //   localStorage Retrieval
+    // ==========================
+
+    // Grab planner content from localStorage
+    var plannerContent = localStorage.getItem("plannerContent");
+
+    // If there's nothing there
+    if (plannerContent === null) {
+        
+        // make plannerContent an array
+        plannerContent = [];
+
+        // Create an empty hour object for each hour
+        for (i = 0; i < 24; i++) {
+            plannerContent.push({
+                hour:i,
+                content:""
+            });
+        } 
+        // Otherwise we just parse the object
+    } else {
+        plannerContent = JSON.parse(plannerContent);
+    }
+
+    // ===========================
+    //    Create HTML Structure
+    // ===========================
+
+    
     for (i = startTime; i < endTime; i++) {
 
         // create row and columns 
         var newRow = $("<div>", {"class": 'row'});
 
         var dateCol = $("<div>", {"class": 'col-md-3'});
-        var contentCol = $("<div>", {"class": 'col-md-9'});
         var saveCol = $("<div>", {"class": 'col-md-3'});
+        
+        // For the content colum nwe also assign the text
+        var contentCol = $("<div>", {"class": 'col-md-9'}).text(plannerContent[i].content);
 
+        // Add save icon to save column
+        saveColCol.append($("<i>"), {"class": "fas fa-save"});
+        
         // =======================
         //       Assign hour
         // =======================
+
         var hourContent;
         if (i === 0 || i === 12) {
             hourContent = "12";
@@ -35,22 +71,13 @@ function populatePlanner(startTime, endTime) {
 
         dateCol.text(hourContent);
 
-        // ========================
-        //  Assign planner content
-        // ========================
-
-        // Grab planner content from localStorage
-        var plannerContent = localStorage.getItem("plannerContent");
-
-        // If there's nothing there
-        if (plannerContent === null) {
-            
-        }
 
 
-
-
-
+        
+        
+        
+        
+        
         // Append up the chain
         newRow.append(dateCol);
         newRow.append(contentCol);
@@ -58,8 +85,4 @@ function populatePlanner(startTime, endTime) {
         
         $(".container").append(newRow);
     }
-
-
-
-
 }
